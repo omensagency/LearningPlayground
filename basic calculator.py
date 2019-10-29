52880d12418bce974dc45d1a07e4c6ababc0e9b6
omensagency <ryan@omens.agency>
2019-10-28 23:05:56 -0700

    Up to Section 3.17

--- basic calculator.py
+++ basic calculator.py
@@ -3,7 +3,42 @@ b = 30
 
 print(a / b)
 
-
 a = "cat"
 
 print(a)
+
+my_number = 29
+
+print(my_number)
+
+
+a = 10
+
+print(type(a))
+
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income * tax_rate
+
+print(my_taxes)
+
+#this is a comment
+print(
+'sing quotes\n'
+"double quotes\n"
+"double quotes with space"
+)
+
+print(len("hello world"))
+
+mystring = "New Hello World"
+
+print(mystring[0:4])
+
+print(mystring[4:-6:2])
+
+print(mystring[::-1])
+
+#Section 3:17 10-28
